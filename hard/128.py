2216ea2b315e2ff7f86da82c6ce20ab727a2608b
Maksim Zubkov <penguin@ntbk.localdomain>
2014-01-03 01:23:13 +0400

    [11/8/13] Challenge #128 [Hard] Mon Petit Fourier

--- hard/128.py
+++ hard/128.py
@@ -1,5 +1,6 @@
 import math
 import sys
+import numpy
 
 def read():
 	points = {}
@@ -19,6 +20,7 @@ if __name__ == '__main__':
 	
 	#print points
 
+	# three different compute of DFT
 	for m in range(N):
 		n = 0
 		real.append(0.0)
@@ -26,24 +28,34 @@ if __name__ == '__main__':
 		complex.append(0.0)
 		for x in sorted(points):
 			y = points[x]
+
+			# trigonometric form of dft
 			real[m] += y*math.cos(2*math.pi*m*n/N)
-			img[m] += y*math.sin(2*math.pi*m*n/N)
+			img[m] -= y*math.sin(2*math.pi*m*n/N)
+			
+			#complex form of dft
 			complex[m] += y*math.e**(-2j*math.pi*n*m/N)
 			n += 1
 		
-		print "x(%d) = %f%+fj" % (m, real[m], -img[m]), complex[m]
+		#print "x(%d) = %f%+fj" % (m, real[m], img[m]), complex[m]
 
-	signal = []
-	for n in range(N):
-		signal.append(0.0)
-		m = 0
-		while  m < N:
-			signal[n] += complex[m]*math.e**(2j*n*m*math.pi/N)
-			m += 1
-		signal[n] /= N
-		#print signal[n]
-		amplitude = math.sqrt(real[n]**2 + img[n]**2)/N
-		phase = math.atan2(img[n], real[n])
-		#print "amplitude: %f, phase: %f, complex form:" % (amplitude, phase), signal[n]
-		sys.stdout.write("%+f*sin(%d*t%+f)" % (amplitude, n, phase))
-		#sys.stdout.write("%+f*sin(2*pi*%d*x%+f)" % (amplitude, n, phase))
+	# dft fron numpy package
+	dft = numpy.fft.fft([points[x] for x in sorted(points)])
+
+	# find three differences
+	#for idx in range(len(dft)):
+	#	print dft[idx], complex[idx], real[idx], img[idx]
+
+	# creation of an analytical equation
+	equation = ''
+
+	for k in range(N):
+		amplitude = math.sqrt(complex[k].real**2 + complex[k].imag**2)/N
+		phase = math.atan2(complex[k].imag, complex[k].real)
+
+		# freq for k numbered point calculated like
+		# f = k/T where T is number of points
+		equation += "%+f*cos(%f*x%+f)" % (amplitude, 2.0*math.pi*k/10.0, phase)
+
+	print "Analytical form: %s" % equation
+	# try to create equation formed as A*sin(a*x+fi_1)+B*cos(b*x+fi_2) 
